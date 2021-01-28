ad6691f23bc6d5ee793f5e3cb47e6df699d5d524
sitnikov-andrey <sitnik54@gmail.com>
2021-01-28 19:24:51 +0300

    Update MyGroupsViewController.swift

--- GeekBrainsUILesson/Groups/MyGroupsViewController.swift
+++ GeekBrainsUILesson/Groups/MyGroupsViewController.swift
@@ -29,11 +29,8 @@ class MyGroupsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            // Если была нажата кнопка «Удалить»
             if editingStyle == .delete {
-            // Удаляем город из массива
                 myGroups.remove(at: indexPath.row)
-            // И удаляем строку из таблицы
                 tableView.deleteRows(at: [indexPath], with: .automatic)
             }
         }
